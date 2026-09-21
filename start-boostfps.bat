fc29e5f2bb44dca2487f2283c0924c347d8ed613
Reflexeiei885 <fff130357@gmail.com>
2026-09-21 12:20:50 +0700

    Delete start-boostfps.bat

--- start-boostfps.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell.exe -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ''%~dp0local-bridge.ps1'''"
-endlocal
